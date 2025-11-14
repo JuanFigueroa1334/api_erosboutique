945dc43ee3f74f77653a566c1e7a6452d7155ea6
Juan Figueroa <ajuanjfigueroa@gmail.com>
2025-11-13 19:54:46 -0500

    correccion de ejecucion en railway

--- src/app.py
+++ src/app.py
@@ -1,19 +1,29 @@
 from flask import Flask
 from flask_cors import CORS
 from src.config import config
-#routes
 from src.routes import usuario
 
-app=Flask(__name__)
-CORS(app, resources={r"*": {"origins": "*"}})
+def create_app():
+    app = Flask(__name__)
+    
+    # CORS permitido correctamente
+    CORS(app, resources={r"*": {"origins": "*"}})
 
-def page_not_found(error):
-    return "<h1>Not found page</h1>", 404
-
-if __name__ == '__main__':
+    # Cargar configuración
     app.config.from_object(config['development'])
-    
+
+    # Registrar rutas
     app.register_blueprint(usuario.main, url_prefix='/api/usuario')
+
+    # Errores
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return "<h1>Not found page</h1>", 404
     
-    app.register_error_handler(404,page_not_found)
+    return app
+
+# Flask CLI con local run
+app = create_app()
+
+if __name__ == "__main__":
     app.run()
