aa65fdb4543038f0d9463d15e50ac72de4642ee9
Juan Figueroa <ajuanjfigueroa@gmail.com>
2025-11-14 13:54:04 -0500

    ajuste para solucionar en produccion

--- src/database/db.py
+++ src/database/db.py
@@ -3,13 +3,15 @@ from psycopg2 import DatabaseError
 from decouple import config
 
 def get_connection():
+    print("Intentando conexión a PostgreSQL...")
     try:
         return psycopg2.connect(
             host=config('PGSQL_HOST'),
             user=config('PGSQL_USER'),
             password=config('PGSQL_PASSWORD'),
             database=config('PGSQL_DATABASE'),
-          
+            port=config('PGSQL_PORT', default=5432)
         )
     except DatabaseError as ex:
+        print("Error al conectar con PostgreSQL:", ex)
         raise ex
